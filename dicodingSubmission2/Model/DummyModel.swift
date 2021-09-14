7cc37c7e5f4edb0aad87b68fd32ac9ee8d952dae
Maitri Vira <maitrivira1@gmail.com>
2021-09-14 10:13:50 +0700

    clean model dummy

---
+++ dicodingSubmission2/Model/DummyModel.swift
@@ -0,0 +1,22 @@
+//
+//  DummyModel.swift
+//  dicodingSubmission2
+//
+//  Created by Maitri Vira on 14/09/21.
+//
+
+let dummyResults = Results(id: 100, name: "Contoh", released: "Contoh", rating: 1.1, backgroundImage: "Contoh", genres: [], tags: [])
+
+let dummyGameData = GameData()
+
+let dummyData = [
+    Results(id: 1, name: "Contoh", released: "Contoh", rating: 1.1, backgroundImage: "Contoh", genres: [], tags: []),
+    Results(id: 1, name: "Contoh", released: "Contoh", rating: 1.1, backgroundImage: "Contoh", genres: [], tags: []),
+    Results(id: 1, name: "Contoh", released: "Contoh", rating: 1.1, backgroundImage: "Contoh", genres: [], tags: []),
+    Results(id: 1, name: "Contoh", released: "Contoh", rating: 1.1, backgroundImage: "Contoh", genres: [], tags: []),
+    Results(id: 1, name: "Contoh", released: "Contoh", rating: 1.1, backgroundImage: "Contoh", genres: [], tags: [])
+]
+
+let dummyGenre = [
+    Genres(id: 1, name: "Contoh")
+]
